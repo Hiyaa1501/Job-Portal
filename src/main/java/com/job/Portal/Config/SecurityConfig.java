7929cc48c9135288a325627ac7b85fb57c463e72
hiyaa1501 <hiyabhakta@gmail.com>
2026-08-26 10:48:36 +0530

    Run security filter before username/password

--- src/main/java/com/job/Portal/Config/SecurityConfig.java
+++ src/main/java/com/job/Portal/Config/SecurityConfig.java
@@ -1,16 +1,28 @@
 package com.job.Portal.Config;
 
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@AllArgsConstructor
 @Configuration
 public class SecurityConfig {
 
+    private final JwtFilter jwtFilter;
+    //This will run jwt filter before username/password
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf(csrf -> csrf.disable()).authorizeHttpRequests(auth -> auth.requestMatchers("/user/**").permitAll().anyRequest().permitAll());
+        httpSecurity.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/user/**").permitAll()
+                        .anyRequest().authenticated())
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+                //Run our JWT filter before Spring's username/password filter
+
         return httpSecurity.build();
     }
 }
